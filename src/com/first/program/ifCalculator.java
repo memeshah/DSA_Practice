4ab3d810ac883e579ff0e918194c9b42f554e2f0
memeshah <shahmeet2911@gmail.com>
2025-03-26 15:10:33 +0530

    If Else problem Commit

---
+++ src/com/first/program/ifCalculator.java
@@ -0,0 +1,26 @@
+package com.first.program;
+
+import java.util.Scanner;
+
+public class ifCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter 2 number to perform calculation");
+        float a = input.nextFloat();
+        float b = input.nextFloat();
+        System.out.println("Please provide an operator you want to perform");
+        String operator = input.next();
+        if(operator.equals("+")){
+            System.out.println(a+" + "+  b + " = " + (a+b));
+        } else if (operator.equals("-")) {
+            System.out.println(a+" - "+  b + " = " + (a-b));
+        } else if (operator.equals("*")) {
+            System.out.println(a+" * "+  b + " = " + (a*b));
+        } else if (operator.equals("/")) {
+            System.out.println(a+" / "+ b+ " = " + (a/b));
+        }else {
+            System.out.println("Invalid Operator has been added.");
+        }
+    }
+}
