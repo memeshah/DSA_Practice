62bcbbed3d642a9435943f59e6a2310ff69700cb
memeshah <shahmeet2911@gmail.com>
2025-03-29 18:06:22 +0530

    Fibonacci

---
+++ src/com/Kunal/conditionalloops/FibonacciSeries.java
@@ -0,0 +1,23 @@
+package com.Kunal.conditionalloops;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+
+
+    System.out.println("Enter n to generate Fibonacci Series.");
+    Scanner input = new Scanner(System.in);
+
+    int n = input.nextInt();
+    int first = 0;
+    int second = 1;
+        System.out.println(first+"\n"+second);
+        for (int i = 2; i < n; i++) {
+            int nextterm = second + first;
+            first = second;
+            second = nextterm;
+            System.out.println(second);
+        }
+    }
+}
